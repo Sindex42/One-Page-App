610f0d141a41895a3bb073580cb314ba53c3f2bc
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-17 15:06:38 +0000

    Reverse order of messages

--- button.js
+++ button.js
@@ -1,13 +1,11 @@
 var button = document.getElementById('button')
-
 var myTextarea = document.getElementById('myTextarea')
-
 var messageLog = document.getElementById('messageLog')
 
 var messageArray = []
 
 function printMessages () {
-  return messageArray.join('<br />')
+  return messageArray.reverse().join('<br />')
 }
 
 button.onclick = function () {
